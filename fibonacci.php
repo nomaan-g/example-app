0759ef97badb17c6c64e683acf37396691fcc3e8
nomaan <nomaan.gagan@gmail.com>
2023-11-17 16:07:03 +0530

    Update fibonacci program for readability and maintainability

--- fibonacci.php
+++ fibonacci.php
@@ -1,22 +1,29 @@
 <?php
-function generateFibonacci($n) {
-    $fibonacci = array();
-    $fibonacci[0] = 0;
-    $fibonacci[1] = 1;
 
-    for ($i = 2; $i < $n; $i++) {
-        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+/**
+ * A Fibonacci function in PHP.
+ *
+ * @param int $n The position in the Fibonacci sequence to calculate.
+ * @return int The Fibonacci number at the given position.
+ */
+function fibonacci($n) {
+    if ($n < 0) {
+        return "Undefined for negative positions";
+    } elseif ($n == 0) {
+       return 0;
+    } elseif ($n == 1) {
+         return 1;
+    } else {
+        return fibonacci($n - 1) + fibonacci($n - 2);
     }
-
-    return $fibonacci;
 }
 
-// Change the number of terms as needed
-$terms = 10;
-$fibonacciSequence = generateFibonacci($terms);
 
-echo "Fibonacci sequence of the first $terms terms:\n";
-foreach ($fibonacciSequence as $number) {
-    echo $number . " ";
-}
+$position = 7; // Change this position to test different Fibonacci numbers
+$result = fibonacci($position);
+
+
+echo "The Fibonacci number at position $position is: $result";
+
 ?>
+
